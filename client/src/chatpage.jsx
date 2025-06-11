750f3c669de0f8e521b920c2b1299f7b72a0eb3d
Ryan Jewik <jewik@chapman.edu>
2025-06-11 04:34:12 -0700

    commented out database stuff in jsx files, needs to be done only in the backend.

--- client/src/chatpage.jsx
+++ client/src/chatpage.jsx
@@ -4,6 +4,34 @@ import { ReactComponent as UserPFP } from "./img/user-profile-pic.svg";
 import { ReactComponent as ViperPFP } from "./img/viper-profile-pic.svg";
 import { ReactComponent as SettingsBtn } from "./img/settings-btn.svg";
 
+/*
+//connect to mongoDB and fetch chat history
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = "mongodb+srv://ryanjewik:7Ku3pYQtCJerX59x@cluster0.0drkzoy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri,  {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    }
+);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+*/
+
+
 
 //demo messages to simulate a chat history
 const demoMessages = [
